f9307555d4ea7eb222242d9509f3fad3f3c4bbd3
Er. Ashok Devatwal <ashokdev78@gmail.com>
2025-03-08 23:06:46 +0530

    Update QueryLog.php

--- src/Models/QueryLog.php
+++ src/Models/QueryLog.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class QueryLog extends Model
 {
-    protected $fillable = ['query', 'bindings', 'time'];
+    protected $fillable = ['query', 'bindings', 'time', 'executed_at'];
+
+    public static function detectFrequentQueries($threshold = 5, $interval = 10)
+    {
+        return DB::table('query_logs')
+            ->select('query', DB::raw('COUNT(*) as count'))
+            ->where('executed_at', '>=', now()->subSeconds($interval))
+            ->groupBy('query')
+            ->having('count', '>=', $threshold)
+            ->get();
+    }
 }

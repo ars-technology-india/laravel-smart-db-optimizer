4c0f30e26070b8718c637f951bfdaa9993806e4f
Er. Ashok Devatwal <ashokdev78@gmail.com>
2025-03-08 23:12:31 +0530

    Update smartdb.php

--- src/config/smartdb.php
+++ src/config/smartdb.php
@@ -4,4 +4,5 @@ return [
     'slow_query_threshold' => 100, // Queries taking more than 100ms
     'frequent_query_threshold' => 5, // Number of times a query runs in short time to be flagged
     'frequent_query_time_window' => 60, // Time window in seconds to track repeated queries
+    'auto_cache' => true, // Enable or disable automatic caching of frequent queries
 ];

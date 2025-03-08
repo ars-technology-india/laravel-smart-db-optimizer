7d2503f4e39c824af813ba9c790319303a293a5b
Er. Ashok Devatwal <ashokdev78@gmail.com>
2025-03-08 21:57:56 +0530

    Update QueryLogger.php

--- src/Middleware/QueryLogger.php
+++ src/Middleware/QueryLogger.php
@@ -11,6 +11,14 @@ class QueryLogger
     public function handle($request, Closure $next)
     {
         DB::listen(function ($query) {
+            // Convert the query to lowercase to avoid case sensitivity issues
+            $sql = strtolower($query->sql);
+        
+            // Ignore queries that interact with the query_logs table
+            if (str_contains($sql, 'query_logs')) {
+                return;
+            }
+        
             if ($query->time > config('smartdb.slow_query_threshold')) {
                 QueryLog::create([
                     'query' => $query->sql,
